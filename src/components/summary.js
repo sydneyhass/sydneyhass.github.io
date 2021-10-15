3f32ffcfbc658a5b6b318c87e51464ab31af4c65
Sydney <sydney.kiersten@gmail.com>
2021-10-14 21:08:32 -0400

    Updated css

--- src/components/summary.js
+++ src/components/summary.js
@@ -4,7 +4,7 @@ const Summary = ({ data }) => (
   <section className="py-5 border-b border-neutral-300 lg:flex items-center">
     <div className="my-5">
       <img
-        className="rounded-full mx-auto w-32 lg:w-full xl:w-4/5"
+        className="rounded-full mx-auto w-64 lg:w-full xl:w-4/5"
         src="./profile.jpg"
         alt="profile"
       />
